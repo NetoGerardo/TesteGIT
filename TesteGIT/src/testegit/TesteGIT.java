93ad92322bf4781c0f816d0bab9c97d709441333
Neto gerardo <neto-1997@Live.com.pt>
2017-09-11 21:34:47 -0300

    Testando commit.2 Fix #1

--- TesteGIT/src/testegit/TesteGIT.java
+++ TesteGIT/src/testegit/TesteGIT.java
@@ -16,6 +16,7 @@ public class TesteGIT {
      */
     public static void main(String[] args) {
         System.out.println("Testando!!!!");
+        System.out.println("Finalizando os testes!!!!");
     }
     
 }
